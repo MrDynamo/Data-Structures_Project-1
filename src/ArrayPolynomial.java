63e31ca156320c97f54e2ed79ee02871292679c2
Walker Morgan <MrDynamoMLG@gmail.com>
2020-09-17 12:36:52 -0500

    Version 1.1
    Implement degree(), displayPolynomial(), and default constructor

--- src/ArrayPolynomial.java
+++ src/ArrayPolynomial.java
@@ -1,18 +1,26 @@
 public class ArrayPolynomial implements Polynomial {
 
-    // Github test
-
     // Initialize array to size 100
-    private double[] polyArray = new double[100];
+    private double[] polyArray;
 
     // Default constructor
     public ArrayPolynomial() {
-
+        polyArray = new double[100];
     }
 
     @Override
     public int degree() {
-        return 0;
+        int deg = 0;
+
+        // Iterate through array
+        for (int i = 0; i < polyArray.length; i++) {
+            // If number at index != 0, set degree to highest index
+            if (polyArray[i] != 0.0) {
+                deg = i;
+            }
+        }
+
+        return deg;
     }
 
     @Override
@@ -48,6 +56,22 @@ public class ArrayPolynomial implements Polynomial {
     // Check indexes of array, print largest index = degree
     @Override
     public void displayPolynomial() {
+        String display = "", tmp = "";
+
+        // Loop through array and add each portion of the polynomial to the display result
+        for (int i = 0; i < polyArray.length; i++) {
+            if (polyArray[i] != 0.0) {
+                tmp = polyArray[i] + "^" + i + " ";
+            }
+
+            display = tmp + display;
+        }
+
+        // Display result if not blank, else display 0.0
+        if (!display.isBlank())
+            System.out.println(display);
+        else
+            System.out.println("0.0");
 
     }
 }
