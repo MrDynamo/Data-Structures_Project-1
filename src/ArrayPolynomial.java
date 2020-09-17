adcd1704a48fb3932c8e9761a1c0986b41c22d69
Walker Morgan <MrDynamoMLG@gmail.com>
2020-09-17 00:53:52 -0500

    Version 1.0
    Base project

---
+++ src/ArrayPolynomial.java
@@ -0,0 +1,51 @@
+public class ArrayPolynomial implements Polynomial {
+
+    // Initialize array to size 100
+    private double[] polyArray = new double[100];
+
+    // Default constructor
+    public ArrayPolynomial() {
+
+    }
+
+    @Override
+    public int degree() {
+        return 0;
+    }
+
+    @Override
+    public double getCoefficient(int power) throws ExponentOutOfRangeException {
+        return 0;
+    }
+
+    @Override
+    public void setCoefficient(double newCoefficient, int power) throws ExponentOutOfRangeException {
+
+    }
+
+    @Override
+    public Polynomial add(Polynomial p) {
+        return null;
+    }
+
+    @Override
+    public Polynomial mult(Polynomial p) throws ExponentOutOfRangeException {
+        return null;
+    }
+
+    @Override
+    public void mult(double scalar) {
+
+    }
+
+    @Override
+    public double evaluate(double x) {
+        return 0;
+    }
+
+    // Check indexes of array, print largest index = degree
+    @Override
+    public void displayPolynomial() {
+
+    }
+}
---
+++ src/ExponentOutOfRangeException.java
@@ -0,0 +1,11 @@
+// ********************************************************
+// ExponentOutOfRangeException class for the ADT Polynomial.
+// By Chi-Cheng Lin
+// Date: 09/07/2020
+// ********************************************************
+public class ExponentOutOfRangeException 
+            extends IndexOutOfBoundsException {
+  public ExponentOutOfRangeException(String s) {
+    super(s);
+  }  // end constructor
+}  // end ExponentOutOfRangeException
