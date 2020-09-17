91948d819a72b7b1df4ec2b694269f67fc82af6a
Walker Morgan <mrdynamomlg@gmail.com>
2020-09-17 16:09:13 -0500

    Version 6.1
    Cleanup code and add extra documentation
    
    ALL TEST CASES PASSED!

--- src/ArrayPolynomial.java
+++ src/ArrayPolynomial.java
@@ -1,47 +1,45 @@
 public class ArrayPolynomial implements Polynomial {
 
-    /*** Variables ***/
+    /*** Class Variables ***/
 
-    // Initialize array to size 100
+    // Declare new array to initalize later
     private double[] coeff;
 
-    /*** Methods ***/
+    /*** Class Methods ***/
 
-    // Default constructors
+    // Default constructor
     public ArrayPolynomial() {
         coeff = new double[100];
     }
 
+    // Constructor for usage in mult(Polynomial) function
     public ArrayPolynomial(double[] coeffArray) {
         coeff = coeffArray;
     }
 
-    // Returns an instance of the coefficient array
-    public double[] getCoeffArray(Polynomial p) {
-        double[] coeffArr = this.coeff;
-        return coeffArr;
-    }
-
-    // Print degree of polynomial (highest index)
+    // Print degree of polynomial
     @Override
     public int degree() {
         int deg = 0;
 
         // Iterate through array
         for (int i = 0; i < coeff.length; i++) {
-            // If number at index != 0, set degree to highest index
+            // If number at index != 0, set degree to index
             if (coeff[i] != 0.0) {
                 deg = i;
             }
         }
+        // Returns highest index, AKA degree
         return deg;
     }
 
     // Return coefficient of given power
     @Override
     public double getCoefficient(int power) throws ExponentOutOfRangeException {
+        // Throw exception degree > 100 (out of range)
         if (power > 100)
             throw new ExponentOutOfRangeException("Maximum power cannot exceed 100");
+        // Else return coefficient at given power
         else
             return coeff[power];
     }
@@ -49,8 +47,10 @@ public class ArrayPolynomial implements Polynomial {
     // Set coefficient of given power
     @Override
     public void setCoefficient(double newCoefficient, int power) throws ExponentOutOfRangeException {
+        // Throw exception if degree > 100 (out of range)
         if (power > 100)
             throw new ExponentOutOfRangeException("Maximum power cannot exceed 100");
+        // Else set the coefficient to new coefficient
         else
             coeff[power] = newCoefficient;
     }
@@ -58,29 +58,35 @@ public class ArrayPolynomial implements Polynomial {
     // Add this polynomial to Polynomial p, return new polynomial with result
     @Override
     public Polynomial add(Polynomial p) {
+        // Declare and initialize new polynomial for result
         Polynomial result = new ArrayPolynomial();
 
         for (int i = 0; i < coeff.length; i++) {
-            result.setCoefficient(coeff[i] + p.getCoefficient(i), i);
+            // Set coefficient of result to the sum of this and p
             result.setCoefficient(coeff[i] + p.getCoefficient(i), i);
         }
 
+        // Return the new polynomial containing the result
         return result;
     }
 
     // Multiply this polynomial by Polynomial p, return new polynomial with result
     @Override
     public Polynomial mult(Polynomial p) throws ExponentOutOfRangeException {
+        // Declare and initialize new double array for product
         double[] result = new double[100];
 
+        // Throw exception if degree > 100 (out of range)
         if (this.degree() + p.degree() > 100)
             throw new ExponentOutOfRangeException("Maximum power cannot exceed 100");
         else {
+            // Iterate through both polynomials, multiplying monomials as you go
             for (int i = 0; i <= this.degree(); i++) {
                 for (int j = 0; j <= p.degree(); j++) {
                     result[i + j] += this.coeff[i] * p.getCoefficient(j);
                 }
             }
+            // Return new polynomial using the result array
             return new ArrayPolynomial(result);
         }
     }
@@ -88,11 +94,14 @@ public class ArrayPolynomial implements Polynomial {
     // Multiply this polynomial by a scalar
     @Override
     public void mult(double scalar) {
+        // Declare and initialize new double array
         double[] blank = new double[100];
 
+        // If multiplying by 0.0, result is 0.0
         if (scalar == 0.0)
             coeff = blank;
         else {
+            // Else iterate through polynomial and multiply each term by the scalar
             for (int i = 0; i < this.degree(); i++) {
                 coeff[i] = coeff[i] * scalar;
             }
@@ -102,27 +111,30 @@ public class ArrayPolynomial implements Polynomial {
     // Evaluate the polynomial at x using Horner's method
     @Override
     public double evaluate(double x) {
+        // Declare and initialize result variable
         double result = 0.0;
 
+        // Use Horner's method to evauluate recursively
         for (int i = 1; i < coeff.length; i++) {
             result += coeff[i] * Math.pow(x, i);
         }
 
+        // Return the result of evauluation at x
         return result;
     }
 
     // Display the full polynomial with formatting
     @Override
     public void displayPolynomial() {
+        // Declare and initialize display strings
         String display = "", tmp = "";
 
-        // Add 0 degree to display
+        // Add 0th degree to display
         if (coeff[0] != 0.0) {
-            //System.out.print("debug 1");
             display = Double.toString(coeff[0]);
         }
 
-        // Add 1 degree to display
+        // Add 1st degree to display
         if (coeff[1] != 0.0) {
             if (coeff[0] == 0.0)
                 display = coeff[1] + "x^1";
@@ -132,9 +144,9 @@ public class ArrayPolynomial implements Polynomial {
             }
         }
 
-        // Loop through array and add each portion of the polynomial to the display result
+        // Loop through array starting at 2nd degree and add each portion of the polynomial to the display result
         for (int i = 2; i < coeff.length; i++) {
-            // Continue adding to display if coefficient != 0.0
+            // Only add to display if coefficient is not 0.0
             if (coeff[i] != 0.0) {
                 // If nothing has been added to display yet, remove the + from the end of display
                 if (display.isBlank())
@@ -147,9 +159,10 @@ public class ArrayPolynomial implements Polynomial {
             }
         }
 
-        // Display result if not blank, else display 0.0
+        // Display result if not blank
         if (!display.isBlank())
             System.out.println(display);
+        // Else display 0.0
         else
             System.out.println("0.0");
 
